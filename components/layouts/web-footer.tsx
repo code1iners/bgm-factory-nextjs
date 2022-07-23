b201c54c32f30db05cfe60ab56e722e7efe9c684
__coma__ <codeliner96@gmail.com>
2022-07-23 12:55:43 +0900

    [v1.10.0] Updated footer text and refactoring component.

---
+++ components/layouts/web-footer.tsx
@@ -0,0 +1,10 @@
+import packageConfig from "@/root/package.json";
+
+export default function WebFooter() {
+  return (
+    <footer className="px-10 py-5 flex flex-col justify-center items-center gap-2">
+      <span className="footer-text">Provided by Codeliners</span>
+      <span className="footer-text">v{packageConfig.version}</span>
+    </footer>
+  );
+}
--- components/layouts/web-layout.tsx
+++ components/layouts/web-layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
-import packageConfig from "@/root/package.json";
 import WebHeader from "@/components/web-header";
 import MainMenu from "@/components/main-menu";
 import { isMenuOpenedAtom } from "@/libs/clients/atoms";
+import WebFooter from "./web-footer";
 
 interface WebLayoutProps {
   children: React.ReactNode;
@@ -21,10 +21,7 @@ const WebLayout = ({ children }: WebLayoutProps) => {
         ) : (
           <div className="flex flex-col h-full overflow-y-auto divide-y">
             <div className="grow">{children}</div>
-            <footer className="px-10 py-5 flex flex-col justify-center items-center gap-2">
-              <span className="footer-text">Codeliners</span>
-              <span className="footer-text">v{packageConfig.version}</span>
-            </footer>
+            <WebFooter />
           </div>
         )}
       </main>
