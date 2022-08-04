a52d3004812e13fca2c9f64f2a562810a212441c
김원이 <codeliner96@fixelsoft.com>
2022-08-04 10:21:21 +0900

    [v1.12.8] Add _blank noopener option.

--- utils/route-utils.ts
+++ utils/route-utils.ts
@@ -5,5 +5,5 @@
 export const openYoutube = (category?: string | string[] | undefined) => {
   let bgm = `${category ? `${category}` : ""}+bgm`;
   const endpoint = `/results?search_query=${bgm.trim()}`;
-  window.open(`https://www.youtube.com${endpoint}`, "_blank");
+  window.open(`https://www.youtube.com${endpoint}`, "_blank", "noopener");
 };
