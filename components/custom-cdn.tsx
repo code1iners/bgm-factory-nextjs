e16fb17bf2420f9bfff2902a47e17c23967baf76
김원이 <codeliner96@fixelsoft.com>
2022-08-04 16:02:43 +0900

    [v1.13.1] Refactoring cdn code.

---
+++ components/custom-cdn.tsx
@@ -0,0 +1,11 @@
+export default function CustomCdn() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.statically.io/gh/code1iners/ce1pers-content-provider-gulp/v0.0.0/dist/mouse-click-effects/index.min.css"
+      />
+    </>
+  );
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,13 @@
-import Favicon from "@/components/favicon";
 import { Html, Head, Main, NextScript } from "next/document";
+import Favicon from "@/components/favicon";
+import CustomCdn from "@/components/custom-cdn";
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <Favicon />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdn.statically.io/gh/code1iners/ce1pers-content-provider-gulp/v0.0.0/dist/mouse-click-effects/index.min.css"
-        />
+        <CustomCdn />
       </Head>
       <body>
         <Main />
