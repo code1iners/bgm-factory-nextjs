483c9693cb50fecb9d9dbe72729c9a338c29aa98
김원이 <codeliner96@fixelsoft.com>
2022-08-04 10:36:51 +0900

    [v1.12.9] Append _blank open options.

--- utils/route-utils.ts
+++ utils/route-utils.ts
@@ -5,5 +5,10 @@
 export const openYoutube = (category?: string | string[] | undefined) => {
   let bgm = `${category ? `${category}` : ""}+bgm`;
   const endpoint = `/results?search_query=${bgm.trim()}`;
-  window.open(`https://www.youtube.com${endpoint}`, "_blank", "noopener");
+  const newWindow = window.open(
+    `https://www.youtube.com${endpoint}`,
+    "_blank",
+    "noopener,noreferrer"
+  );
+  if (newWindow) newWindow.opener = null;
 };
